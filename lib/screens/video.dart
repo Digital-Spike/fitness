4489caa76b5760bf7f2aa850a2c0a358120e63e0
Digital Spikes <digitalspikestechnology.com>
2023-06-12 18:32:26 -0800

    video screen

--- lib/screens/video.dart
+++ lib/screens/video.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Video extends StatefulWidget {
-  const Video({super.key});
+  const Video({Key? key});
 
   @override
   State<Video> createState() => _VideoState();
@@ -12,7 +12,14 @@ class _VideoState extends State<Video> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Video'),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text(
+          'Video',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
