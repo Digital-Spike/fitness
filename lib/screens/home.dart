8d43c6d5d6041232b8899c7cccc02bb22a650172
Digital Spikes <digitalspikestechnology.com>
2023-06-12 18:30:59 -0800

    home screen appbar updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,207 +51,214 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.black,
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(color: Colors.white),
+        leading: IconButton(
+          icon: Icon(Icons.language),
+          color: Colors.white,
+          onPressed: () {
+            // Handle language icon press
+          },
+        ),
         title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              height: 100,
-              decoration: BoxDecoration(
-                boxShadow: [],
-              ),
-              child: Image.asset(
-                'assets/fitnessname.png',
-                height: 80,
-              ),
-            ),
-            Spacer(),
-            IconButton(
-              icon: Icon(Icons.notifications),
-              onPressed: () {
-                // Handle notification button press
-              },
+            Image.asset(
+              'assets/fitnessname.png',
+              height: AppBar().preferredSize.height + 35, // Increase the height
             ),
           ],
         ),
-      ),
-      drawer: Drawer(
-        child: Container(
-          color: Colors.black12,
-          child: ListView(
-            children: [
-              // Drawer items...
-            ],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            color: Colors.white,
+            onPressed: () {
+              // Handle notification icon press
+            },
           ),
-        ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CarouselSlider(
-              options: CarouselOptions(
-                height: 200.0,
-                viewportFraction: 1.0,
-                enlargeCenterPage: false,
-                autoPlay: true,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentCarouselIndex = index;
-                  });
-                },
-              ),
-              items: carouselImages.map((image) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10.0),
-                        boxShadow: [
-                          BoxShadow(
+      body: Column(
+        children: [
+          Divider(
+            height: 1,
+            color: Colors.white,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CarouselSlider(
+                    options: CarouselOptions(
+                      height: 200.0,
+                      viewportFraction: 1.0,
+                      enlargeCenterPage: false,
+                      autoPlay: true,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          _currentCarouselIndex = index;
+                        });
+                      },
+                    ),
+                    items: carouselImages.map((image) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset(0, 2),
+                                  blurRadius: 6.0,
+                                ),
+                              ],
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0),
+                              child: Image.asset(
+                                image,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: carouselImages.map((image) {
+                      int index = carouselImages.indexOf(image);
+                      return Container(
+                        width: 8.0,
+                        height: 8.0,
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 2.0),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentCarouselIndex == index
+                              ? Colors.blueAccent
+                              : Colors.grey,
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Select a Trainer for EMS at Home',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
                             color: Colors.black26,
                             offset: Offset(0, 2),
                             blurRadius: 6.0,
                           ),
                         ],
                       ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: Image.asset(
-                          image,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: carouselImages.map((image) {
-                int index = carouselImages.indexOf(image);
-                return Container(
-                  width: 8.0,
-                  height: 8.0,
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: _currentCarouselIndex == index
-                        ? Colors.blueAccent
-                        : Colors.grey,
+                    ),
                   ),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Select a Trainer for EMS at Home',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                      blurRadius: 6.0,
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: trainersList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final trainerImage = trainersList[index];
+                        return GestureDetector(
+                          onTap: () {
+                            // Handle trainer selection
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border:
+                                  Border.all(color: Colors.grey, width: 1.5),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                'https://fitnessjourni.com/api/uploads/' +
+                                    (data?[index]["image"] ?? ""),
+                                // 'https://fitnessjourni.com/api/uploads/$trainerImage',
+                                // fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 200,
-              width: double.infinity,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: trainersList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final trainerImage = trainersList[index];
-                  return GestureDetector(
-                    onTap: () {
-                      // Handle trainer selection
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.grey, width: 1.5),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          'https://fitnessjourni.com/api/uploads/' +
-                              (data?[index]["image"] ?? ""),
-                          // 'https://fitnessjourni.com/api/uploads/$trainerImage',
-                          // fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Select a Trainer for EMS at Gym',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                      blurRadius: 6.0,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              height: 200,
-              width: double.infinity,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: trainersList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final trainerImage = trainersList[index];
-                  return GestureDetector(
-                    onTap: () {
-                      // Handle trainer selection
-                    },
-                    child: Container(
-                      margin: EdgeInsets.only(right: 10),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.grey, width: 1.5),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          'https://fitnessjourni.com/api/uploads/' +
-                              (data?[index]["image"] ?? ""),
-                          // 'https://fitnessjourni.com/api/uploads/$trainerImage',
-                          // fit: BoxFit.cover,
-                        ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Select a Trainer for EMS at Gym',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 2),
+                            blurRadius: 6.0,
+                          ),
+                        ],
                       ),
                     ),
-                  );
-                },
+                  ),
+                  Container(
+                    height: 200,
+                    width: double.infinity,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: trainersList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final trainerImage = trainersList[index];
+                        return GestureDetector(
+                          onTap: () {
+                            // Handle trainer selection
+                          },
+                          child: Container(
+                            margin: EdgeInsets.only(right: 10),
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border:
+                                  Border.all(color: Colors.grey, width: 1.5),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                'https://fitnessjourni.com/api/uploads/' +
+                                    (data?[index]["image"] ?? ""),
+                                // 'https://fitnessjourni.com/api/uploads/$trainerImage',
+                                // fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
