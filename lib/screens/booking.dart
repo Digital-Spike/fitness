433b5f3af666099b9bee551e10edf4fc38a820e0
digitalspike <digitalspikeapps@gmail.com>
2023-06-25 23:49:29 +0530

    Booking details

--- lib/screens/booking.dart
+++ lib/screens/booking.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fitness/constants/api_list.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Booking extends StatefulWidget {
   const Booking({super.key});
@@ -8,92 +13,182 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
+  Future<bool>? futureData;
+  Map<String, dynamic>? bookingData;
+  User? user = FirebaseAuth.instance.currentUser;
+
+  @override
+  void initState() {
+    futureData = fetchData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.black,
-        body: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Card(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Center(
-                    child: Text(
-                      'Booking Confirmed',
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700),
+        body: FutureBuilder<bool>(
+            future: futureData,
+            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Center(
+                            child: Text(
+                              'Booking Confirmed',
+                              style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Trainer : ',
+                                  textAlign: TextAlign.end,
+                                  style: TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Text(
+                                  ' ${bookingData?['trainerName']}',
+                                  style: const TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Id :',
+                                  textAlign: TextAlign.end,
+                                  style: TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Text(
+                                  ' ${bookingData?['bookingId']}',
+                                  style: const TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Date :',
+                                  textAlign: TextAlign.end,
+                                  style: TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Text(
+                                  ' ${bookingData?['bookingDate']}',
+                                  style: const TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            children: [
+                              const Expanded(
+                                flex: 2,
+                                child: Text(
+                                  'Time :',
+                                  textAlign: TextAlign.end,
+                                  style: TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 4,
+                                child: Text(
+                                  ' ${bookingData?['bookingTime']}',
+                                  style: const TextStyle(
+                                      fontFamily: 'Roboto',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          const Center(
+                            child: Text(
+                              'Thank You For Choosing\nFitness Journey',
+                              style: TextStyle(
+                                  fontFamily: 'Roboto',
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w700),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Name               :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Booking Id       :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Booking Date  :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Booking Time  :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Trainer Name  :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Branch Name  :',
-                    style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Spacer(),
-                  Center(
-                    child: Text(
-                      'Thank You For Choosing\nFitness Journey',
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ));
+                );
+              }
+
+              if (snapshot.hasError) {
+                const Text("Something wrong");
+              }
+
+              return const Center(child: CircularProgressIndicator());
+            }));
+  }
+
+  Future<bool> fetchData() async {
+    try {
+      String trainerUrl = "${ApiList.apiUrl}getUserBookings.php";
+      var response =
+          await http.post(Uri.parse(trainerUrl), body: {'userId': user?.uid});
+      bookingData = json.decode(response.body);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
