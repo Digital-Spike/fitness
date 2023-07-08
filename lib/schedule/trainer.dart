fc6da89168980bec36a90115c14a3bcc3a17425c
digitalspike <digitalspikeapps@gmail.com>
2023-07-08 10:10:01 +0530

    Trainer list warning fix

--- lib/schedule/trainer.dart
+++ lib/schedule/trainer.dart
@@ -1,28 +1,19 @@
 import 'package:fitness/schedule/trainerprofile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-
 
 class Trainer extends StatefulWidget {
   const Trainer({super.key});
-
- 
-
- 
 
   @override
   State<Trainer> createState() => _TrainerState();
 }
 
 class _TrainerState extends State<Trainer> {
- 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffF1F1F2),
-       appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: const Color(0xffF1F1F2),
         elevation: 0,
         bottom: PreferredSize(
@@ -35,66 +26,68 @@ class _TrainerState extends State<Trainer> {
         title: const Text(
           'Our Trainers',
           style: TextStyle(
-            fontSize:18,fontWeight:FontWeight.w900,color: Colors.black),
+              fontSize: 18, fontWeight: FontWeight.w900, color: Colors.black),
         ),
         centerTitle: true,
-        leading: BackButton(color: Colors.black),
+        leading: const BackButton(color: Colors.black),
       ),
-      body: 
-        
-        Container(
-          padding: EdgeInsets.all(10),
-          child: GridView.builder(
-           
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
             shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               mainAxisExtent: 280,
-            
-             crossAxisSpacing: 10,
+              crossAxisSpacing: 10,
               crossAxisCount: 2,
-              ),
-           
-           
-                  itemCount: 6,
-                  
-                  itemBuilder: (context, index) {
-                    return  GestureDetector(
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>TrainerProfile()));
-                      },
-                      child: Stack(
+            ),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const TrainerProfile()));
+                },
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 270,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              image: AssetImage('assets/IMG_9750.png'),
+                              fit: BoxFit.cover)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Container(
-                         
-                            height: 270,
-                           
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(10),
-                             image: DecorationImage(image: AssetImage('assets/IMG_9750.png'),fit: BoxFit.cover)
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              
-                              children: [
-                                Row(
-                                  children: [
-                                    Container(height: 30,width: 8,color: Colors.deepOrange,),SizedBox(width: 20),Text('Trainer Name',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w900,color: Colors.white)),
-                                    
-                                  ],
-                                ),
-                                SizedBox(height: 20,)
-                              ],
-                            ),
+                          Row(
+                            children: [
+                              Container(
+                                height: 30,
+                                width: 8,
+                                color: Colors.deepOrange,
+                              ),
+                              const SizedBox(width: 20),
+                              const Text('Trainer Name',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.white)),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
                           )
-                            
                         ],
                       ),
-                    );
-                   } ),
-        ),
-        
- 
+                    )
+                  ],
+                ),
+              );
+            }),
+      ),
     );
   }
 }
