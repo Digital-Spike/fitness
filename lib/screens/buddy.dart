3f79b2795139a9ce0e73ab621779534901c39a55
Shashiraj333 <shashirajreddy@gmail.com>
2023-08-29 19:37:54 +0530

    new payment api implemented

--- lib/screens/buddy.dart
+++ lib/screens/buddy.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:fitness/screens/packagedetail.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
@@ -11,98 +13,149 @@ class Buddy extends StatefulWidget {
 }
 
 class _BuddyState extends State<Buddy> {
-   List<int> session = [2, 4, 8, 12, 24, 48, 96];
-  List<int> price = [398, 796, 1552, 2328, 4296, 6240, 9600];
-  List<int> perSession = [199, 199, 194, 194, 179, 130, 100];
-  List<int> validity = [
-   15 ,
-    30 ,
-    60 ,
-    90 ,
-    160 ,
-    180 ,
-    365 
-  ];
+  List<Map<String, dynamic>> plansList = [];
+  Future<void>? futureData;
   final List<Gradient> gridGradients = [
     LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.red.shade900,  Colors.red.shade200,]),
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.red.shade900,  Colors.red.shade200,]),
     LinearGradient(
-       begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [  Colors.green.shade900,Colors.green.shade200]),
- LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [  Colors.cyan.shade900,  Colors.cyan.shade200,]),
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [  Colors.green.shade900,Colors.green.shade200]),
     LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.orange.shade900,  Colors.orange.shade200,]),
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [  Colors.cyan.shade900,  Colors.cyan.shade200,]),
     LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.purple.shade900,  Colors.purple.shade200,]),
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.orange.shade900,  Colors.orange.shade200,]),
     LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.yellow.shade900,  Colors.yellow.shade200,]),
-       LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.teal.shade900,  Colors.teal.shade200,]),
-       LinearGradient(
-      begin: Alignment.bottomRight,
-      end: Alignment.topLeft,
-      colors: [ Colors.pink.shade900,  Colors.pink.shade200,]),
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.purple.shade900,  Colors.purple.shade200,]),
+    LinearGradient(
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.yellow.shade900,  Colors.yellow.shade200,]),
+    LinearGradient(
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.teal.shade900,  Colors.teal.shade200,]),
+    LinearGradient(
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+        colors: [ Colors.pink.shade900,  Colors.pink.shade200,]),
   ];
- List<int> specialsession = [24, 48, 96, ];
-  List<int> specialprice = [3866, 5616, 8640, ];
-  List<String> specialperSession = ['NA'];
-  List<String> specialvalidity = [
-   '3 months',
-    '6 months',
-    '12 months'
-    
-  ];
-  
+  @override
+  void initState() {
+    super.initState();
+    futureData = getBranchList();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Padding(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          
-          itemCount: session.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            mainAxisExtent: 120,
-            crossAxisCount: 2), itemBuilder: ((context, index) {
-          return Container(
-            
-            padding: EdgeInsets.all(5),
-            margin: EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-             gradient: gridGradients[index]
-           
-            ),
-            child: Column(
-              children: [
-               Text(session[index].toString(),style: TextStyle(fontSize: 50,fontWeight: FontWeight.bold),),
-              
-               Text('Sessions',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)
-                  ],
+        child: FutureBuilder<void>(
+          future: futureData,
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return GridView.builder(
+                itemCount: plansList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisExtent: 120,
+                  crossAxisCount: 2,
                 ),
-                
-              
-            
-          );
-        })),
+                itemBuilder: ((context, index) {
+                  var package = plansList[index];
+                  String validity = package['validity'];
+                  int days = int.tryParse(validity.split(' ')[0]) ?? 0;
+                  return InkWell(
+                    onTap: () {
+                      var selectedPackage = plansList[index];
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PackageDetail(
+                            gradient: gridGradients[index],
+                            price: int.parse(selectedPackage['price']),
+                            session: int.parse(selectedPackage['sessions']),
+                            validity: days,
+                            perSession: selectedPackage['price_session'].toString(),
+                          ),
+                        ),
+                      );
+
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        gradient: gridGradients[index],
+                      ),
+                      child: Column(
+                        children: [
+                          Text(
+                            package['sessions'].toString(),
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Sessions',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text("Something went wrong"));
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
 
- 
+  Future<void> getBranchList() async {
+    try {
+      String trainerUrl = "https://fitnessjourni.com/api/getAllPackages";
+      var response = await http.post(Uri.parse(trainerUrl), body: {});
+
+      if (response.statusCode == 200) {
+        print("API Response: ${response.body}");
+        List<Map<String, dynamic>> allPackages = (json.decode(response.body) as List<dynamic>)
+            .map((package) => package as Map<String, dynamic>)
+            .toList();
+
+       
+        plansList = allPackages
+            .where((package) => package['persons'] == 'buddy')
+            .toList();
+      }
+    } catch (e) {
+      print("Error: $e");
+    }
+
+    
+    setState(() {});
+  }
+
 }
